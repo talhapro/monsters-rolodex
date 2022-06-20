4be2be47a2742ebb5b31f3756225ef9f88013aaa
Talha Solo <talha@soloking.pro>
2022-06-20 05:06:38 +0500

    commit on master

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
+import { Component } from 'react';
+
+import CardList from './components/card-list/card-list.component';
+import SearchBox from './components/search-box/search-box.component';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor() {
+    super();
+
+    this.state = {
+       monsters: [],
+       searchField: ''
+    }
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then((response) => response.json())
+    .then((users) =>
+      this.setState(
+      () => {
+        return {monsters: users}
+      },
+      () => {
+        
+      }
+    ));
+  }
+  
+  onKeySearch = (event) => {
+    
+    const searchField = event.target.value.toLowerCase();
+
+    this.setState(() => {
+      return { searchField }
+    });
+  }
+
+
+  render () {
+
+    const {monsters, searchField} = this.state;
+    const { onKeySearch } = this;
+
+    const filteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLowerCase().includes(searchField);
+    });
+
+    return (
+     
+      <div className="App">
+        <SearchBox 
+          className = 'monster-search-box'
+          onChangeHandler = {onKeySearch}
+          placeholder={'search monsters'}
+        />
+        <CardList monsters = {filteredMonsters}/ >
+
+      </div>
+    );
+  }
 }
+
 
 export default App;
